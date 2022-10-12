8fdd09067beb80d2c911a9dafa563e73bd7c25e6
Alexandre Carnevali <alexandrecar@yahoo.com.br>
2022-10-12 12:29:03 -0300

    fix: teste IRremote funcionando

--- src/Tests/TESTE-IR/TESTE-IR.ino
+++ src/Tests/TESTE-IR/TESTE-IR.ino
@@ -1,34 +1,21 @@
 //Tirei as infos daqui: http://www.esp32learning.com/code/esp32-and-infrared-receiver-example.php
 //E daqui: https://www.pjrc.com/teensy/td_libs_IRremote.html
-#include <IRremote.h>
+#include <IRremote.hpp>
  
-#define JCONTROLLER 15
+#define JCONTROLLER 4
  
 IRrecv irrecv(JCONTROLLER);
 decode_results results;
  
 void setup() {
-  Serial.begin(9600);
+  pinMode(JCONTROLLER, INPUT);
+  Serial.begin(115200);
   irrecv.enableIRIn(); // Start the receiver
 }
  
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX); //Verifica o valor lido pelo controle
-
-    //Parece que também dá pra testar pra ver o tipo de controle!
-    if (results.decode_type == NEC) {
-      Serial.print("NEC: ");
-    } else if (results.decode_type == SONY) {
-      Serial.print("SONY: ");
-    } else if (results.decode_type == RC5) {
-      Serial.print("RC5: ");
-    } else if (results.decode_type == RC6) {
-      Serial.print("RC6: ");
-    } else if (results.decode_type == UNKNOWN) {
-      Serial.print("UNKNOWN: ");
-    }
-
     irrecv.resume();
   }
 }
