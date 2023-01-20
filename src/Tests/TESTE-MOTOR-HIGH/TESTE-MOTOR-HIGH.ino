18016256c7164f52e0530f5159dfef0eee727483
ALFiora <analuiza197@gmail.com>
2023-01-20 18:55:37 -0300

    feat: novos testes

--- src/Tests/TESTE-MOTOR-HIGH/TESTE-MOTOR-HIGH.ino
+++ src/Tests/TESTE-MOTOR-HIGH/TESTE-MOTOR-HIGH.ino
@@ -4,8 +4,12 @@
 void setup(){
   Serial.begin(115200);
 
-  digitalWrite(MOTOR_L, HIGH);
+  pinMode(MOTOR_R, OUTPUT); //ESC1
   digitalWrite(MOTOR_R, HIGH);
+  pinMode(MOTOR_L, OUTPUT); //ESC2
+  digitalWrite(MOTOR_L, HIGH);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
 }
 
 void loop() {
