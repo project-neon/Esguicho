ff9f7cbea422186a2b080f3b2f9bb6242da54ebe
ALFiora <analuiza197@gmail.com>
2023-03-18 13:04:48 -0300

    deleting: chaging files place - Software

--- src/Tests/MotorsTests/TESTE-TRACAO/utils.h
+++
@@ -1,9 +0,0 @@
-void printSpeed() {
-  // Mostra o valor que será enviado ao map
-  Serial.print("speedL: ");
-  Serial.print(speedL);
-  Serial.print("\t");
-  Serial.print("speedR: ");
-  Serial.print(speedR); 
-  Serial.println("\t\t");
-}
--- src/Tests/TESTE-INSPECAO/_config.h
+++
@@ -1,9 +0,0 @@
-//TODO: Mudar o nome do arquivo pra pinout.h?
-#include <Arduino.h>
-
-//motors (left, right)
-#define MOTOR_L 16
-#define MOTOR_R 17
-
-//JUDGE CONTROLLER
-#define JCONTROLLER 4
